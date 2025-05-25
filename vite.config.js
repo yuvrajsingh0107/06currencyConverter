c92fed368a846137871f4f0fc4bc731d0d85bbe4
yuvraj singh sisodiya <uvrajput0107@gmail.com>
2025-05-26 01:41:11 +0530

    bug fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/CurrencyConvertor/',
+  base: '/CurrencyConverter/',
   plugins: [react(),tailwindcss()],
 })
